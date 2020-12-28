697c0c39a957d5e17c90213ec6bd9eed9d26f3bb
Hanna Tiara Andarlia <hannatiara@gmail.com>
2020-12-29 03:46:11 +0700

    Complete the home page view

---
+++ src/javascript/home.js
@@ -0,0 +1,11 @@
+/*
+    Copyright (C) 2020-2021 Hanna Tiara Andarlia
+*/
+
+// Import Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, far, fab)
