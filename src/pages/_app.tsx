bc242e5e0c4d4aebf1b08bbd9f6a3f1dc7cd59ce
Hanna Tiara Andarlia <hannatiara@gmail.com>
2023-09-11 15:51:43 +0700

    Set up next repo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import 'src/styles/globals.css'
-import type { AppProps } from 'next/app'
+import 'styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Inter } from 'next/font/google';
+import Script from 'next/script';
+
+const inter = Inter({ subsets: ['latin'], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'] });
+const iconId = process.env.NEXT_PUBLIC_FONTAWESOME_ID;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={inter.className}>
+      <Script src={`https://kit.fontawesome.com/${iconId}.js`} />
+      <Component {...pageProps} />
+    </main>
+  );
 }
