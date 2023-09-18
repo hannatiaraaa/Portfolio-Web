d9ecf05a3e20ee8e2d8ef3d52e4788fff24bbae5
Hanna Tiara Andarlia <hannatiara@gmail.com>
2023-09-18 21:04:38 +0700

    Update font import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,8 @@
 import 'styles/globals.css';
 import type { AppProps } from 'next/app';
-import { Inter } from 'next/font/google';
 import Script from 'next/script';
+import { inter } from 'configs/fonts';
 
-const inter = Inter({ subsets: ['latin'], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'] });
 const iconId = process.env.NEXT_PUBLIC_FONTAWESOME_ID;
 
 export default function App({ Component, pageProps }: AppProps) {
